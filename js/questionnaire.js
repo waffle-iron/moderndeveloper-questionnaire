71899dfa14d488766ea389ff69da5035a3bda200
Gabriele Romeo <gabriele.romeo@gmail.com>
2016-07-14 17:02:03 +0200

    Fix general indentation

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -1,132 +1,132 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function (e) {
-  function Questionnaire(element) {
-    this.element = element;
-    this.init();
-  };
+    function Questionnaire(element) {
+        this.element = element;
+        this.init();
+    };
 
-  Questionnaire.prototype = {
-    constructor: Questionnaire,
+    Questionnaire.prototype = {
+        constructor: Questionnaire,
 
-    init: function () {
+        init: function () {
 
-        // Checks for valid email addresses
-        var regx = ['^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)',
-                    '|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+',
-                    '[^<>()[\]\.,;:\s@\"]{2,})$'].join('');
+            // Checks for valid email addresses
+            var regx = ['^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)',
+                        '|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+',
+                        '[^<>()[\]\.,;:\s@\"]{2,})$'].join('');
 
-        this.questionnairePrefix = "lmd-";
-        this.questionnaireName = this.questionnairePrefix + 'questionnaire';
-        this.storage = sessionStorage;
+            this.questionnairePrefix = "lmd-";
+            this.questionnaireName = this.questionnairePrefix + 'questionnaire';
+            this.storage = sessionStorage;
 
-        // Object containing patterns for form validation
-        this.requiredFields = {
-            email: {
-                value: new RegExp(regx, 'i')
-            },
+            // Object containing patterns for form validation
+            this.requiredFields = {
+                email: {
+                    value: new RegExp(regx, 'i')
+                },
 
-            empty: {
-                value: ''
-            }
-      };
-
-        this.createQuestionnaire();
-        this.handleSubmitCardForm();
-    },
-
-    createQuestionnaire: function () {
-        var questionnaireObj = {};
-        questionnaireObj['items'] = [];
-
-        if (!this.storage.getItem(this.questionnaireName)) {
-            this.storage.setItem(this.questionnaireName, 
-                                 JSON.stringify(questionnaireObj));
-        }
-    },
-
-    handleSubmitCardForm: function () {
-        var self = this;
-
-        // Creates a single event listener on the grandparent node (article)
-        this.element.addEventListener('click', function (e) {
-
-            var target = e.target;
-
-            if (target.nodeName.toLowerCase() === 'button') {
-                // Here e.target.form is a reference to the target form card
-                self._validateCardForm(target.form); 
-
-                if (target.form.checkValidity()) {
-                    self._saveCardFormData(target.form);
-                } else {
-                    console.log('Error in form');
+                empty: {
+                    value: ''
                 }
+          };
+
+            this.createQuestionnaire();
+            this.handleSubmitCardForm();
+        },
+
+        createQuestionnaire: function () {
+            var questionnaireObj = {};
+            questionnaireObj['items'] = [];
+
+            if (!this.storage.getItem(this.questionnaireName)) {
+                this.storage.setItem(this.questionnaireName, 
+                                     JSON.stringify(questionnaireObj));
             }
+        },
 
-            e.preventDefault();  // Prevents default behaviour
-            e.stopPropagation(); // Stops Event Bubbling 
-        });
-    },
+        handleSubmitCardForm: function () {
+            var self = this;
 
-    _validateCardForm: function (card) {
+            // Creates a single event listener on the grandparent node (article)
+            this.element.addEventListener('click', function (e) {
 
-        var self = this;
+                var target = e.target;
 
-        var fields = self.requiredFields,
-            inputs = card.querySelectorAll('input');
+                if (target.nodeName.toLowerCase() === 'button') {
+                    // Here e.target.form is a reference to the target form card
+                    self._validateCardForm(target.form); 
 
-        map(inputs, function(input) {
-            var errorMsgEmpty   = input.getAttribute('data-error-empty'),
-                errorMsgInvalid = input.getAttribute('data-error-invalid');
-
-            if (input.required) {
-                if (!input.value) {
-                    input.setCustomValidity(errorMsgEmpty);
-                } else {
-                    input.setCustomValidity('');
+                    if (target.form.checkValidity()) {
+                        self._saveCardFormData(target.form);
+                    } else {
+                        console.log('Error in form');
+                    }
                 }
-            }
 
-            self._handleValidationError(input);
-        });
-    },
+                e.preventDefault();  // Prevents default behaviour
+                e.stopPropagation(); // Stops Event Bubbling 
+            });
+        },
 
-    _handleValidationError: function (field) {
+        _validateCardForm: function (card) {
 
-        var msgContainer = void 0;
+            var self = this;
 
-        var parent = field.parentNode,
-            hasError = parent.classList.contains('has-error'),
-            errorMessage = parent.querySelector('.error-message');
+            var fields = self.requiredFields,
+                inputs = card.querySelectorAll('input');
 
-        if (field.checkValidity()) {
-            if (hasError) {
-                parent.classList.remove('has-error');
-                parent.removeChild(errorMessage);
-            }
-        } else {
-            if (!hasError) {
-                msgContainer = document.createElement('div');
-                msgContainer.className = 'error-message';
-                msgContainer.innerHTML = field.validationMessage;
-                parent.classList.add('has-error');
-                parent.appendChild(msgContainer);
+            map(inputs, function(input) {
+                var errorMsgEmpty   = input.getAttribute('data-error-empty'),
+                    errorMsgInvalid = input.getAttribute('data-error-invalid');
+
+                if (input.required) {
+                    if (!input.value) {
+                        input.setCustomValidity(errorMsgEmpty);
+                    } else {
+                        input.setCustomValidity('');
+                    }
+                }
+
+                self._handleValidationError(input);
+            });
+        },
+
+        _handleValidationError: function (field) {
+
+            var msgContainer = void 0;
+
+            var parent = field.parentNode,
+                hasError = parent.classList.contains('has-error'),
+                errorMessage = parent.querySelector('.error-message');
+
+            if (field.checkValidity()) {
+                if (hasError) {
+                    parent.classList.remove('has-error');
+                    parent.removeChild(errorMessage);
+                }
             } else {
-                errorMessage.innerHTML = field.validationMessage;
+                if (!hasError) {
+                    msgContainer = document.createElement('div');
+                    msgContainer.className = 'error-message';
+                    msgContainer.innerHTML = field.validationMessage;
+                    parent.classList.add('has-error');
+                    parent.appendChild(msgContainer);
+                } else {
+                    errorMessage.innerHTML = field.validationMessage;
+                }
             }
+        },
+
+        _saveCardFormData: function (card) {
+            console.log('Form saved');
         }
-    },
+    };
 
-    _saveCardFormData: function (card) {
-      console.log('Form saved');
-    }
-  };
-
-  (function(e) {
-    var element = e.target.querySelector('.js--questionnaire');
-    var questionnaire = new Questionnaire(element);
-  })(e);
+    (function(e) {
+        var element = e.target.querySelector('.js--questionnaire');
+        var questionnaire = new Questionnaire(element);
+    })(e);
 });
 
 function map(array, callback) {
